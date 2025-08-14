6d184bfd36a53cda5aa17bca3736c9849f3684d5
adbenitez <asieldbenitez@gmail.com>
2025-08-14 16:35:45 +0200

    update script

--- scripts/generate.py
+++ scripts/generate.py
@@ -3,6 +3,10 @@ import os
 import sys
 
 
+def sort(sentences: dict[str, int]) -> list[int, str]:
+    return sorted((s[1], s[0]) for s in sentences.items())
+
+
 def sort0(pair: tuple[int, str]) -> int:
     words_count = len(pair[1].split())
     if words_count <= 2:
@@ -37,42 +41,50 @@ def main() -> None:
     start = 0
     count = 100000
 
-    sentences = {}
-    meanings = {}
-    meanings_ids = {}
+    sentences: dict[str, int] = {}
+    sentences_ids: set[int] = set()  # for counting duplicates
+    meanings: dict[str, list[str]] = {}
+    meanings_ids: dict[str, int] = {}
     with open(source) as fd:
         for line in fd.readlines():
             row = line.strip().split("\t")
             id = int(row[0])
-            sentences[id] = row[1]
-            meanings.setdefault(id, []).append(row[3])
-            meanings_ids[row[3]] = int(row[2])
+            sentences_ids.add(id)
+            sentence = row[1]
+            if sentence not in sentences or sentences[sentence] > id:
+                sentences[sentence] = id
+            mlist = meanings.setdefault(sentence, [])
+            meaning = row[3]
+            if meaning not in mlist:
+                mlist.append(meaning)
+                meanings_ids[meaning] = int(row[2])
 
-    print(f"TOTAL: {len(sentences)}")
+    print(f"TOTAL: {len(sentences_ids)}")
+    print(f"UNIQUE: {len(sentences)}")
     count = min(len(sentences), count)
 
     new_sents = []
-    items = sorted(sorted(sentences.items()), key=sort0)
+    items = sorted(sort(sentences), key=sort0)
     for id, sen in list(items)[:100]:
         new_sents.append((id, sen))
-        del sentences[id]
-    items = sorted(sorted(sentences.items()), key=sort1)
+        del sentences[sen]
+    items = sorted(sort(sentences), key=sort1)
     for id, sen in list(items)[:500]:
         new_sents.append((id, sen))
-        del sentences[id]
-    items = sorted(sorted(sentences.items()), key=sort2)
+        del sentences[sen]
+    items = sorted(sort(sentences), key=sort2)
     for id, sen in list(items)[:10000]:
         new_sents.append((id, sen))
-        del sentences[id]
-    items = sorted(sorted(sentences.items()), key=sort3)
+        del sentences[sen]
+    items = sorted(sort(sentences), key=sort3)
     for id, sen in list(items):
         new_sents.append((id, sen))
-        del sentences[id]
+        del sentences[sen]
 
     if len(sys.argv) == 2 and sys.argv[1] == "sort":
         with open(source, "w") as f:
             for id, sen in new_sents:
-                for meaning in meanings[id]:
+                for meaning in meanings[sen]:
                     mid = meanings_ids[meaning]
                     f.write(f"{id}\t{sen}\t{mid}\t{meaning}\n")
         print(f"sorted {len(new_sents)} sentences")
@@ -83,7 +95,7 @@ def main() -> None:
         count2 = 0
         for id, sen in new_sents[start:count]:
             count2 += 1
-            f.write(f"{sen}\t{'|'.join(meanings[id])}".replace("`", "\`"))
+            f.write(f"{sen}\t{'|'.join(meanings[sen])}".replace("`", "\`"))
             if count2 != count:
                 f.write("\n")
         f.write('`.split("\\n");\n')
